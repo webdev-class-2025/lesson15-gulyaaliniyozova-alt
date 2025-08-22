c6b1d9180892806426df627b1ff87b26a86d183b
Gulya Aliniyozova <gulyaaliniyozova@gmail.com>
2025-08-22 14:11:49 +0500

    My homework

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+// === Карточка товара  ===
+let productName = "Беспровадние наушники ";
+let price = 150.00;
+let discount =10;
+let rating = 4.5;
+let isAvailable =true;
+
+//
+let discountedPrice = price- (price * discount / 100);
+
+//
+let availabilityText = isAvailable ? "В наличии " : "Нет в наличии";
+
+// Вывод информация в консоль
+console.log(" === Карточка товара===");
+console.log( " Название :" , productName);
+console.log(" Цена :" , price.toFixed(2) + "$");
+console.log(" Скидка:" , discount + "%");
+console.log("Цена  со скидкой:", discountedPrice . toFixed(2) +"$");
+console.log("Рейтинг" , rating , "из 5");
+console.log("Наличие :" , availabilityText );
